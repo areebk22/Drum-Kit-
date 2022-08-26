e637c5bd03fe02054eca768814fb29a0303dbef7
areebk22 <100992225+areebk22@users.noreply.github.com>
2022-08-27 02:21:23 +0530

    Add files via upload
    First Upload

---
+++ index.js
@@ -0,0 +1,57 @@
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick001.wav");
+      kick.play();
+      break;
+    default:
+  }
+}
+
+function buttonAnimation(key){
+  var activeButton = document.querySelector("."+key);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){                      // Removes animation after 50milliseconds.
+    activeButton.classList.remove("pressed")
+  }, 50);
+}
+
+//Clicking Buttons on screen
+for (var i=0 ; i<7 ; i++){                                                      // Because 7 instruments
+  document.querySelectorAll("button")[i].addEventListener("click",function () {
+    var key = this.innerHTML  ;
+    makeSound(key);                                       // "w","a","s","d","j","k","l"
+    buttonAnimation(key);
+  });
+}
+
+// Pressing keys.
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
